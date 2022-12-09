fe11917972023460abb09c6253384b37343a7896
José Francisco Jarquin Martínez <ex500791.jose.jarquin@mail.telcel.com>
2022-12-09 12:39:26 -0600

    proyecto vacío

---
+++ app/src/main/java/com/example/cl08_animacin_compartido_entre_activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cl08_animacin_compartido_entre_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
